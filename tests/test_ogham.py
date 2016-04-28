60cbbfd50eac0188322d2009e06723ba2cca8cad
Liam Cooke <git@araile.com>
2016-04-29 00:34:07 +0100

    Add another test

--- tests/test_ogham.py
+++ tests/test_ogham.py
@@ -9,6 +9,17 @@ from giog import ogham
 
 class TestOgham(unittest.TestCase):
 
+    def test_normalise(self):
+        ga_str = 'dia duit'
+        en_str = 'xylophone'  # x and y are not valid characters
+
+        ga_norm = ogham.normalise(ga_str)
+        en_norm = ogham.normalise(en_str)
+
+        self.assertEqual(ga_str, ga_norm)
+        self.assertNotEqual(en_str, en_norm)
+
+
     def test_ogham(self):
         input_str = 'Dia duit, a Dhomhain.'
 
