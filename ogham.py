d0a6a72a68eb12e31b9c7deee92c9623684f9e74
Liam Cooke <git@araile.com>
2016-04-28 23:56:44 +0100

    Refactor and assume Python 3

--- ogham.py
+++ ogham.py
@@ -1,66 +1,71 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+Ogham script
+
+"""
 
 # start and end markers
-format = u'\u169B%s\u169C'
+FORMAT_STR = '\u169B%s\u169C'
 
-chars = {
-    ' ': u'\u1680',
+CHARS = {
+    ' ': '\u1680',
 
-    'b': u'\u1681',
-    'l': u'\u1682',
-    'f': u'\u1683',
-    's': u'\u1684',
-    'n': u'\u1685',
+    'b': '\u1681',
+    'l': '\u1682',
+    'f': '\u1683',
+    's': '\u1684',
+    'n': '\u1685',
 
-    'h': u'\u1686',
-    'd': u'\u1687',
-    't': u'\u1688',
-    'c': u'\u1689',
-    'q': u'\u168A',
+    'h': '\u1686',
+    'd': '\u1687',
+    't': '\u1688',
+    'c': '\u1689',
+    'q': '\u168A',
 
-    'm': u'\u168B',
-    'g': u'\u168C',
-    'z': u'\u168E',
-    'r': u'\u168F',
+    'm': '\u168B',
+    'g': '\u168C',
+    'z': '\u168E',
+    'r': '\u168F',
 
-    'x': u'\u1699',     # ch/x/ae
-    'p': u'\u169A',
+    'x': '\u1699',     # ch/x/ae
+    'p': '\u169A',
 
-    'a': u'\u1690',
-    'o': u'\u1691',
-    'u': u'\u1692',
-    'e': u'\u1693',
-    'i': u'\u1694',
+    'a': '\u1690',
+    'o': '\u1691',
+    'u': '\u1692',
+    'e': '\u1693',
+    'i': '\u1694',
 
-    u'á': u'\u1690',
-    u'ó': u'\u1691',
-    u'ú': u'\u1692',
-    u'é': u'\u1693',
-    u'í': u'\u1694',
+    'á': u'\u1690',
+    'ó': u'\u1691',
+    'ú': u'\u1692',
+    'é': u'\u1693',
+    'í': u'\u1694',
 }
 
 pairs = {
-    'ch': u'\u1699',    # ch/x/ae
-    'ng': u'\u168D',
+    'ch': '\u1699',    # ch/x/ae
+    'ng': '\u168D',
 
-    'ea': u'\u1695',
-    'oi': u'\u1696',
-    'ui': u'\u1697',
-    'io': u'\u1698',
-    'ae': u'\u1699',    # ch/x/ae
+    'ea': '\u1695',
+    'oi': '\u1696',
+    'ui': '\u1697',
+    'io': '\u1698',
+    'ae': '\u1699',    # ch/x/ae
 }
 
+
 def normalise(text):
-    return ''.join(filter(lambda c: c in chars, text.lower()))
+    return ''.join(filter(lambda c: c in CHARS, text.lower()))
+
 
 def ogham(text):
     s = normalise(text)
     for pair, char in pairs.items():
         s = s.replace(pair, char)
-    s = map(lambda c: chars.get(c, c), s)
-    return format % ''.join(s)
+    s = map(lambda c: CHARS.get(c, c), s)
+    return FORMAT_STR % ''.join(s)
+
 
 def test(text):
-    print normalise(text)
-    print ogham(text)
+    print(normalise(text))
+    print(ogham(text))
